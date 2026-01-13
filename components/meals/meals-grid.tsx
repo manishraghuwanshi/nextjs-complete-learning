527cc590e100d6d7ea423d5c97b407c2d9c8a6e6
manishraghuwanshi <manishraghuwanshiofficial@gmail.com>
2026-01-13 23:51:54 +0530

    updated meals and react and react dom installation

--- components/meals/meals-grid.tsx
+++ components/meals/meals-grid.tsx
@@ -1,7 +1,22 @@
-import MealItem from './meal-item';
-import classes from './meals-grid.module.css';
+import MealItem from "./meal-item";
+import classes from "./meals-grid.module.css";
 
-export default function MealsGrid({ meals }) {
+type Meal = {
+  id: string;
+  title: string;
+  slug: string;
+  image: string;
+  summary: string;
+  creator: string;
+};
+
+type MealsGridProps = {
+  meals: Meal[];
+};
+
+export default function MealsGrid({
+  meals,
+}: MealsGridProps): JSX.Element {
   return (
     <ul className={classes.meals}>
       {meals.map((meal) => (
